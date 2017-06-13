b1abf6d6f94eff2ddef841bdad606f3a6d8c78a8
Paula Paysan <paula.paysan@gmail.com>
2017-06-13 20:33:51 +0000

    finished website

--- application_controller.rb
+++ application_controller.rb
@@ -7,7 +7,13 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-
   
-  
+  post '/birthstone' do
+    birth_rock=params[:month]
+    @gem = choose_gem(birth_rock)
+    year=params[:year]
+    @zodiac = zodiac_sign(year)
+    erb :results
+    
+  end
 end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,43 @@
 def choose_gem(month)
-
- 
+    month.downcase
+    if month == "january"
+        stone = "garnet"
+    elsif month == "february"
+        stone = "amethyst"
+    elsif month == "march"
+        stone ="aquamarine"
+    elsif month == "april"
+        stone = "diamond"
+    elsif month == "may"
+        stone = "emerald"
+    elsif month == "june"
+        stone = "pearl"
+    elsif month == "july"
+        stone ="ruby"
+    elsif month == "august"
+        stone = "peridot"
+    elsif month == "september"
+        stone = "sapphire"
+    elsif month == "october"
+        stone = "tourmeline"
+    elsif month == "november"
+        stone ="yellow topaz"
+    elsif month == "december"
+        stone = "turquoise"
+    else
+        stone = "error"
+    stone
+    end
 end
 
+def zodiac_sign(birth_year)
+    birth_year = birth_year.to_i
+    zodiac = ["rat","ox","tiger","rabbit","dragon","snake","horse","goat","monkey","rooster","dog","pig"]
+    x = birth_year - 4
+    b = x/12 
+    a = b*12
+    zodiac_sign = x - a
+    zodiac[zodiac_sign]
+end
+
+puts zodiac_sign(1999)
